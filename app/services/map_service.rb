536155ac39eadfe4bdb521bb69d30f94e673009d
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-09 10:41:25 -0700

    Update map service for distance call

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -6,6 +6,14 @@ class MapService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def self.distance_to_trails(start_longitude, start_latitude, trail_longitude, trail_latitude)
+    response = conn.get("/directions/v2/route") do |r|
+      r.params['from'] = "#{start_latitude},#{start_longitude}"
+      r.params['to'] = "#{trail_latitude},#{trail_longitude}"
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def self.conn
     Faraday.new("http://www.mapquestapi.com") do |f|
       f.params['key'] = ENV['MAPQUEST_KEY']
