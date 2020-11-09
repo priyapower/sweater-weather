d26c7e2dbaf5d41bc5ccb60398e425f7d11cbe9e
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-09 08:32:17 -0700

    Update forecast request spec for complete testing of response

--- spec/requests/api/v1/forecast/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast/forecast_request_spec.rb
@@ -3,72 +3,66 @@ require "rails_helper"
 RSpec.describe "Forecast API", :vcr do
   describe "can send forecast details by location" do
     scenario "can retrive weather by location as index action" do
-      # get long and lat for city
-      # "latLng": {
-      #                   "lat": 39.738453,
-      #                   "lng": -104.984853
-      #               }
-      # openweather onecall api with long and lat from above
-      # Testing should look for more than just the presence of attribute fields in the response. Testing should also determine which fields should NOT be present. (don’t send unnecessary data)
       query_params = {
         location: 'denver, co'
         }
       get "/api/v1/forecast", params: query_params
       expect(response).to be_successful
       forecast = JSON.parse(response.body, symbolize_names: true)[:data]
-      binding.pry
-      expect(forecast[:attributes][:latitude]).to eq(39.738453)
-      expect(forecast[:attributes][:longtitude]).to eq(-104.984853)
-      #
-      # {
-      #   "data": {
-      #     "id": null,
-      #     "type": "forecast",
-      #     "attributes": {
-      #       "current_weather": {
-      #         "datetime": "2020-09-30 13:27:03 -0600",
-      #         "temperature": 79.4,
-      #         etc
-      #       },
-      #       "daily_weather": [
-      #         {
-      #           "date": "2020-10-01",
-      #           "sunrise": "2020-10-01 06:10:43 -0600",
-      #           etc
-      #         },
-      #         {...} etc
-      #       ],
-      #       "hourly_weather": [
-      #         {
-      #           "time": "14:00:00",
-      #           "wind_speed": "4 mph",
-      #           "wind_direction": "from NW",
-      #           etc
-      #         },
-      #         {...} etc
-      #       ]
-      #     }
-      #   }
-      # }
-      # #
-      # expect(forecast[:id]).to be_nil
-      # expect(forecast[:attributes]).to have_key(:date)
-      # expect(forecast[:attributes][:date]).to be_a(String)
-      #
-      # expect(forecast[:attributes]).to have_key(:current_temp)
-      # expect(forecast[:attributes][:current_temp]).to be_a(Float)
-      #
-      # expect(forecast[:attributes]).to have_key(:feels_like)
-      # expect(forecast[:attributes][:feels_like]).to be_a(Float)
-      #
-      # expect(forecast[:attributes]).to have_key(:high_temp)
-      # expect(forecast[:attributes][:high_temp]).to be_a(Float)
-      #
-      # expect(forecast[:attributes]).to have_key(:low_temp)
-      # expect(forecast[:attributes][:low_temp]).to be_a(Float)
-      #
-      # expect(forecast[:attributes]).to have_key(:weather_summary)
-      # expect(forecast[:attributes][:weather_summary]).to be_a(String)
+      current_forecast = forecast[:attributes][:current_weather]
+      expect(current_forecast).to be_a(Hash)
+      expect(current_forecast).to have_key(:datetime)
+      expect(current_forecast[:datetime]).to be_a(String)
+      expect(current_forecast).to have_key(:sunrise)
+      expect(current_forecast[:sunrise]).to be_a(String)
+      expect(current_forecast).to have_key(:sunset)
+      expect(current_forecast[:sunset]).to be_a(String)
+      expect(current_forecast).to have_key(:temp)
+      expect(current_forecast[:temp]).to be_a(Float)
+      expect(current_forecast).to have_key(:feels_like)
+      expect(current_forecast[:feels_like]).to be_a(Float)
+      expect(current_forecast).to have_key(:humidity)
+      expect(current_forecast[:humidity]).to be_a(Numeric)
+      expect(current_forecast).to have_key(:uvi)
+      expect(current_forecast[:uvi]).to be_a(Numeric)
+      expect(current_forecast).to have_key(:conditions)
+      expect(current_forecast[:conditions]).to be_a(String)
+      expect(current_forecast).to have_key(:icon)
+      expect(current_forecast[:icon]).to be_a(String)
+
+      daily_forecast = forecast[:attributes][:daily_weather]
+      expect(daily_forecast).to be_a(Array)
+      expect(daily_forecast.count).to eq(5)
+      expect(daily_forecast.first).to have_key(:date)
+      expect(daily_forecast.first[:date]).to be_a(String)
+      expect(daily_forecast.first).to have_key(:sunrise)
+      expect(daily_forecast.first[:sunrise]).to be_a(String)
+      expect(daily_forecast.first).to have_key(:sunset)
+      expect(daily_forecast.first[:sunset]).to be_a(String)
+      expect(daily_forecast.first).to have_key(:max_temp)
+      expect(daily_forecast.first[:max_temp]).to be_a(Float)
+      expect(daily_forecast.first).to have_key(:min_temp)
+      expect(daily_forecast.first[:min_temp]).to be_a(Float)
+      expect(daily_forecast.first).to have_key(:conditions)
+      expect(daily_forecast.first[:conditions]).to be_a(String)
+      expect(daily_forecast.first).to have_key(:icon)
+      expect(daily_forecast.first[:icon]).to be_a(String)
+
+      hourly_forecast = forecast[:attributes][:hourly_weather]
+      expect(hourly_forecast).to be_a(Array)
+      expect(hourly_forecast.count).to eq(8)
+      expect(hourly_forecast.first).to have_key(:time)
+      expect(hourly_forecast.first[:time]).to be_a(String)
+      expect(hourly_forecast.first).to have_key(:temp)
+      expect(hourly_forecast.first[:temp]).to be_a(Float)
+      expect(hourly_forecast.first).to have_key(:wind_speed)
+      expect(hourly_forecast.first[:wind_speed]).to be_a(Float)
+      expect(hourly_forecast.first).to have_key(:wind_direction)
+      expect(hourly_forecast.first[:wind_direction]).to be_a(String)
+      expect(hourly_forecast.first).to have_key(:conditions)
+      expect(hourly_forecast.first[:conditions]).to be_a(String)
+      expect(hourly_forecast.first).to have_key(:icon)
+      expect(hourly_forecast.first[:icon]).to be_a(String)
     end
   end
 end
