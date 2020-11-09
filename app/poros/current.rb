97f254d97e7913d55ae408a779ec7615a1f89548
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-09 08:36:11 -0700

    Add current poro with spec
    - Saves only datetime, sunrise, sunset, temp, feels like, humidity, uvi, 
    conditions, and icon

---
+++ app/poros/current.rb
@@ -0,0 +1,23 @@
+class Current
+  attr_reader :datetime,
+              :sunrise,
+              :sunset,
+              :temp,
+              :feels_like,
+              :humidity,
+              :uvi,
+              :conditions,
+              :icon
+
+  def initialize(attributes)
+    @datetime = Time.at(attributes[:dt])
+    @sunrise = Time.at(attributes[:sunrise])
+    @sunset = Time.at(attributes[:sunset])
+    @temp = attributes[:temp]
+    @feels_like = attributes[:feels_like]
+    @humidity = attributes[:humidity]
+    @uvi = attributes[:uvi]
+    @conditions = attributes[:weather][0][:description]
+    @icon = attributes[:weather][0][:icon]
+  end
+end
