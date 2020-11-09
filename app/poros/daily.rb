55f9601cd02118438001e1fc05c0a8ce946f01d9
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-09 08:36:36 -0700

    Add daily poro and spec
    - This saves date, sunrise, sunset, max and min temps, conditions, and 
    icon

---
+++ app/poros/daily.rb
@@ -0,0 +1,19 @@
+class Daily
+  attr_reader :date,
+              :sunrise,
+              :sunset,
+              :max_temp,
+              :min_temp,
+              :conditions,
+              :icon
+
+  def initialize(attributes)
+    @date = Time.at(attributes[:dt])
+    @sunrise = Time.at(attributes[:sunrise])
+    @sunset = Time.at(attributes[:sunset])
+    @max_temp = attributes[:temp][:max]
+    @min_temp = attributes[:temp][:min]
+    @conditions = attributes[:weather][0][:description]
+    @icon = attributes[:weather][0][:icon]
+  end
+end
