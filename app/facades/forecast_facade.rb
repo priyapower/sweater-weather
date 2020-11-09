c5381b6bf8fba55d823035b8c2993e08ba9a6a16
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-09 08:34:21 -0700

    Add forecast facade and spec
    The forecast facade aggragates the following data:
    - gets json response from forecast service
    - sends pertinent info to current poro
    - sends pertinent info to daily poro
    - sends pertinent info to hourly poro
    - sends current, daily, and hourly info to forecast poro

---
+++ app/facades/forecast_facade.rb
@@ -0,0 +1,13 @@
+class ForecastFacade
+  def self.forecast_by_coords(map)
+    json = ForecastService.forecast_by_coords(map)
+    current = Current.new(json[:current])
+    daily = json[:daily][0..4].map do |json_daily|
+      Daily.new(json_daily)
+    end
+    hourly = json[:hourly][0..7].map do |json_hourly|
+      Hourly.new(json_hourly)
+    end
+    Forecast.new(current, daily, hourly)
+  end
+end
