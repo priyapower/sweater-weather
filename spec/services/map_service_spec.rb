de4bd12f99c992d374d75dcff0825978d79fc8be
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-10 12:52:35 -0700

    Update map serivce spec after refactor for section1a
    - successful test now tests live data from vcr
    - add unsuccessful test with status code and message live testing

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-RSpec.describe MapService do
-  it "calls external api MapQuest for coordinates", :vcr do
+RSpec.describe MapService, :vcr do
+  it "calls external api MapQuest for coordinates by location" do
     location = 'denver, co'
     json = MapService.get_coords_by_loc(location)
     expect(json).to be_a(Hash)
@@ -14,12 +14,28 @@ RSpec.describe MapService do
     expect(json[:results].first[:locations].first).to have_key(:latLng)
     expect(json[:results].first[:locations].first[:latLng]).to be_a(Hash)
     expect(json[:results].first[:locations].first[:latLng]).to have_key(:lat)
-    expect(json[:results].first[:locations].first[:latLng]).to have_key(:lng)
     expect(json[:results].first[:locations].first[:latLng][:lat]).to be_a(Float)
+    expect(json[:results].first[:locations].first[:latLng][:lat]).to eq(39.738453)
+    expect(json[:results].first[:locations].first[:latLng]).to have_key(:lng)
     expect(json[:results].first[:locations].first[:latLng][:lng]).to be_a(Float)
+    expect(json[:results].first[:locations].first[:latLng][:lng]).to eq(-104.984853)
   end
 
-  it "calls external api MapQuest for distance", :vcr do
+  it "fails to call external api MapQuest for coordinates by location" do
+    location = ''
+    json = MapService.get_coords_by_loc(location)
+    expect(json).to be_a(Hash)
+    expect(json).to have_key(:info)
+    expect(json[:info]).to be_a(Hash)
+    expect(json[:info]).to have_key(:statuscode)
+    expect(json[:info][:statuscode]).to be_a(Integer)
+    expect(json[:info][:statuscode]).to eq(400)
+    expect(json[:info]).to have_key(:messages)
+    expect(json[:info][:messages]).to be_an(Array)
+    expect(json[:info][:messages]).to eq(["Illegal argument from request: Insufficient info for location"])
+  end
+
+  xit "calls external api MapQuest for distance" do
     start_longitude = -104.984853
     start_latitude = 39.738453
     trail_longitude = -104.9567
@@ -32,6 +48,6 @@ RSpec.describe MapService do
     expect(json[:route][:distance]).to be_a(Float)
   end
 
-  xit "calls external api Mapquest for distance for road trip", :vcr do
+  xit "calls external api Mapquest for distance for road trip" do
   end
 end
