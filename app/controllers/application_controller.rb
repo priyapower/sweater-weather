6b2ed2e7389653195ca41f8be8774d4c79941444
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-10 16:33:04 -0700

    Add generate error method inside application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  def generate_error(resource)
+    error_messages = ''
+    resource.errors.messages.each do |validation, message|
+      error_messages += "#{validation.capitalize} (#{message[0]}); "
+    end
+    error_messages = error_messages.delete_suffix('; ')
+    "ERROR: #{error_messages}"
+  end
 end
