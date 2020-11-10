043d46a0831ce90ab18563f32fac2c3193f12a2f
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-10 11:33:54 -0700

    Update section 1a test for vcr use
    - Has new expectations for keys that don't belong in response
    - Has new expectations for live data from vcr

--- spec/requests/api/v1/forecast/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast/forecast_request_spec.rb
@@ -9,8 +9,19 @@ RSpec.describe "Forecast API", :vcr do
       get "/api/v1/forecast", params: query_params
       expect(response).to be_successful
       expect(response.content_type).to eq("application/json")
-      forecast = JSON.parse(response.body, symbolize_names: true)[:data]
-      current_forecast = forecast[:attributes][:current_weather]
+      forecast = JSON.parse(response.body, symbolize_names: true)
+      expect(forecast).to be_a(Hash)
+
+      expect(forecast).to have_key(:data)
+      expect(forecast[:data]).to be_a(Hash)
+      expect(forecast[:data]).to have_key(:id)
+      expect(forecast[:data][:id]).to be_nil
+      expect(forecast[:data]).to have_key(:type)
+      expect(forecast[:data][:type]).to eq('forecast')
+      expect(forecast[:data]).to have_key(:attributes)
+      expect(forecast[:data][:attributes]).to be_a(Hash)
+
+      current_forecast = forecast[:data][:attributes][:current_weather]
       expect(current_forecast).to be_a(Hash)
       expect(current_forecast).to have_key(:datetime)
       expect(current_forecast[:datetime]).to be_a(String)
@@ -19,9 +30,9 @@ RSpec.describe "Forecast API", :vcr do
       expect(current_forecast).to have_key(:sunset)
       expect(current_forecast[:sunset]).to be_a(String)
       expect(current_forecast).to have_key(:temp)
-      expect(current_forecast[:temp]).to be_a(String)
+      expect(current_forecast[:temp]).to be_a(Float)
       expect(current_forecast).to have_key(:feels_like)
-      expect(current_forecast[:feels_like]).to be_a(String)
+      expect(current_forecast[:feels_like]).to be_a(Float)
       expect(current_forecast).to have_key(:humidity)
       expect(current_forecast[:humidity]).to be_a(Numeric)
       expect(current_forecast).to have_key(:uvi)
@@ -30,8 +41,18 @@ RSpec.describe "Forecast API", :vcr do
       expect(current_forecast[:conditions]).to be_a(String)
       expect(current_forecast).to have_key(:icon)
       expect(current_forecast[:icon]).to be_a(String)
+      # DOESN'T RETURN TOO MUCH INFO FROM JSON
+      expect(current_forecast).to_not have_key(:clouds)
+      expect(current_forecast).to_not have_key(:pressure)
+      # TESTING LIVE DATA FROM VCR RESPONSE
+      expect(current_forecast[:temp]).to eq(35.31)
+      expect(current_forecast[:feels_like]).to eq(26.31)
+      expect(current_forecast[:humidity]).to eq(66)
+      expect(current_forecast[:uvi]).to eq(2.5)
+      expect(current_forecast[:conditions]).to eq('mist')
+      expect(current_forecast[:icon]).to eq('50d')
 
-      daily_forecast = forecast[:attributes][:daily_weather]
+      daily_forecast = forecast[:data][:attributes][:daily_weather]
       expect(daily_forecast).to be_a(Array)
       expect(daily_forecast.count).to eq(5)
       expect(daily_forecast.first).to have_key(:date)
@@ -41,21 +62,29 @@ RSpec.describe "Forecast API", :vcr do
       expect(daily_forecast.first).to have_key(:sunset)
       expect(daily_forecast.first[:sunset]).to be_a(String)
       expect(daily_forecast.first).to have_key(:max_temp)
-      expect(daily_forecast.first[:max_temp]).to be_a(String)
+      expect(daily_forecast.first[:max_temp]).to be_a(Float)
       expect(daily_forecast.first).to have_key(:min_temp)
-      expect(daily_forecast.first[:min_temp]).to be_a(String)
+      expect(daily_forecast.first[:min_temp]).to be_a(Float)
       expect(daily_forecast.first).to have_key(:conditions)
       expect(daily_forecast.first[:conditions]).to be_a(String)
       expect(daily_forecast.first).to have_key(:icon)
       expect(daily_forecast.first[:icon]).to be_a(String)
+      # DOESN'T RETURN TOO MUCH INFO FROM JSON
+      expect(current_forecast).to_not have_key(:clouds)
+      expect(current_forecast).to_not have_key(:pressure)
+      # TESTING LIVE DATA FROM VCR RESPONSE
+      expect(daily_forecast.first[:max_temp]).to eq(45.77)
+      expect(daily_forecast.first[:min_temp]).to eq(31.23)
+      expect(daily_forecast.first[:conditions]).to eq('overcast clouds')
+      expect(daily_forecast.first[:icon]).to eq('04d')
 
-      hourly_forecast = forecast[:attributes][:hourly_weather]
+      hourly_forecast = forecast[:data][:attributes][:hourly_weather]
       expect(hourly_forecast).to be_a(Array)
       expect(hourly_forecast.count).to eq(8)
       expect(hourly_forecast.first).to have_key(:time)
       expect(hourly_forecast.first[:time]).to be_a(String)
       expect(hourly_forecast.first).to have_key(:temp)
-      expect(hourly_forecast.first[:temp]).to be_a(String)
+      expect(hourly_forecast.first[:temp]).to be_a(Float)
       expect(hourly_forecast.first).to have_key(:wind_speed)
       expect(hourly_forecast.first[:wind_speed]).to be_a(String)
       expect(hourly_forecast.first).to have_key(:wind_direction)
@@ -64,6 +93,15 @@ RSpec.describe "Forecast API", :vcr do
       expect(hourly_forecast.first[:conditions]).to be_a(String)
       expect(hourly_forecast.first).to have_key(:icon)
       expect(hourly_forecast.first[:icon]).to be_a(String)
+      # DOESN'T RETURN TOO MUCH INFO FROM JSON
+      expect(current_forecast).to_not have_key(:clouds)
+      expect(current_forecast).to_not have_key(:pressure)
+      # TESTING LIVE DATA FROM VCR RESPONSE
+      expect(hourly_forecast.first[:temp]).to eq(35.31)
+      expect(hourly_forecast.first[:wind_speed]).to eq('7.67 mph')
+      expect(hourly_forecast.first[:wind_direction]).to eq('NNE')
+      expect(hourly_forecast.first[:conditions]).to eq('overcast clouds')
+      expect(hourly_forecast.first[:icon]).to eq('04d')
     end
 
     xscenario "sad path for incorrect location or invalid query" do
