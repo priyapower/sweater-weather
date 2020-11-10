0d75f99df23ae94a2b887f2394f0fef78b516205
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-10 12:32:47 -0700

    Refactor forecast controller for sad path
    - If forecast is successful, will return as a forecast poro
    - Else, returns as map poro (currently) with error messages saved to 
    poro

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     forecast = ForecastFacade.forecast_by_coords(query_params[:location])
-    render json: ForecastSerializer.new(forecast)
+    if forecast.class == Map
+      render body: forecast.messages, status: 404
+    else
+      render json: ForecastSerializer.new(forecast)
+    end
   end
 
   private
