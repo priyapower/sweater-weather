0b372780dfc8eca1ad3bf5704ad066055d90121b
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-10 12:18:30 -0700

    Refactor forecast facade
    - takes into account map status code
    - if 400, return error sequence
    - else, complete happy path call

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,15 +1,19 @@
 class ForecastFacade
   def self.forecast_by_coords(location)
     map = MapFacade.get_coords_by_loc(location)
-    json = ForecastService.forecast_by_coords(map)
-    current = Current.new(json[:current])
-    daily = json[:daily][0..4].map do |json_daily|
-      Daily.new(json_daily)
+    if map.status_code == 400
+      map
+    else
+      json = ForecastService.forecast_by_coords(map)
+      current = Current.new(json[:current])
+      daily = json[:daily][0..4].map do |json_daily|
+        Daily.new(json_daily)
+      end
+      hourly = json[:hourly][0..7].map do |json_hourly|
+        Hourly.new(json_hourly)
+      end
+      Forecast.new(current, daily, hourly)
     end
-    hourly = json[:hourly][0..7].map do |json_hourly|
-      Hourly.new(json_hourly)
-    end
-    Forecast.new(current, daily, hourly)
   end
 
   def self.forecast_for_hike(map)
