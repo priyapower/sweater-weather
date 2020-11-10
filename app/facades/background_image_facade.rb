74c3fa6b09639b2f8aeaf8de59fe899e1c72f0d9
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-10 16:01:21 -0700

    Refactor background image facade for sad path errors

--- app/facades/background_image_facade.rb
+++ app/facades/background_image_facade.rb
@@ -1,11 +1,15 @@
 class BackgroundImageFacade
   def self.get_image_by_location(location)
     forecast = ForecastFacade.forecast_by_coords(location)
-    time = time_in_human_speak(forecast.current_weather.datetime)
-    weather = forecast.current_weather.conditions
-    query = "#{time} #{weather} #{location}"
-    json = BackgroundImageService.get_image_by_location(query)
-    BackgroundImage.new(json[:value][0], location)
+    if forecast.class == Map
+      forecast
+    else
+      time = time_in_human_speak(forecast.current_weather.datetime)
+      weather = forecast.current_weather.conditions
+      query = "#{time} #{weather} #{location}"
+      json = BackgroundImageService.get_image_by_location(query)
+      BackgroundImage.new(json[:value][0], location)
+    end
   end
 
   def self.time_in_human_speak(data)
