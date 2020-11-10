6895ef6b1b3750e860ac8fb217bd470f965e163d
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-09 19:43:53 -0700

    Update user model
    - has a callback for api generation
    - validates api key is unique
    - has self method for setting attribute api key
    - has instance method of generating an api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
+  before_create :set_api_key
+  # before_validation :set_api_key, on: :create
   has_secure_password
 
   validates :email, uniqueness: true, presence: true
   validates :password, confirmation: true
+  validates :api_key, uniqueness: true
+
+  def set_api_key
+    self.api_key = generate_api_key
+  end
+
+  def generate_api_key
+    SecureRandom.base58(24)
+  end
 end
