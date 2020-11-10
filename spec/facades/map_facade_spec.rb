478a17b0fc1581d4a95184a94a2f85b67d2e2e51
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-10 12:56:49 -0700

    Update map facade after refactor for section1a

--- spec/facades/map_facade_spec.rb
+++ spec/facades/map_facade_spec.rb
@@ -1,18 +1,22 @@
 require "rails_helper"
 
-RSpec.describe MapFacade do
-  it "aggragates data for coords", :vcr do
+RSpec.describe MapFacade, :vcr do
+  it "aggragates data for coords and returns a map poro" do
     location = 'denver, co'
     map = MapFacade.get_coords_by_loc(location)
     expect(map).to be_a(Map)
     expect(map.id).to be_nil
+    expect(map.status_code).to be_a(Integer)
+    expect(map.status_code).to eq(0)
+    expect(map.messages).to be_a(Array)
+    expect(map.messages).to be_empty
     expect(map.longitude).to be_a(Float)
-    expect(map.latitude).to be_a(Float)
     expect(map.longitude).to eq(-104.984853)
+    expect(map.latitude).to be_a(Float)
     expect(map.latitude).to eq(39.738453)
   end
 
-  it "aggragates data for distance to trails", :vcr do
+  xit "aggragates data for distance to trails" do
     start_longitude = -104.984853
     start_latitude = 39.738453
     trail_longitude = -104.9567
