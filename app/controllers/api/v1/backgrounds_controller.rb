9adc5265144200cd4301cb8b172a13b9d520bfe4
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-10 16:01:10 -0700

    Refactor backgrounds controller for sad path errors

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -2,7 +2,11 @@ class Api::V1::BackgroundsController < ApplicationController
   def index
     location = query_params[:location]
     background_image = BackgroundImageFacade.get_image_by_location(location)
-    render json: ImageSerializer.new(background_image)
+    if background_image.class == Map
+      render body: background_image.messages, status: 404
+    else
+      render json: ImageSerializer.new(background_image)
+    end
   end
 
   private
