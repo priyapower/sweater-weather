1e87fa4c48b584371aa4c5171475ef50a71df8db
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-10 19:45:22 -0700

    Update hourly spec for updates to convert degrees to direction
    - Fixes math inside converting degrees to direction
    - Tests all cardinal directions in spec test

--- app/poros/hourly.rb
+++ app/poros/hourly.rb
@@ -18,10 +18,6 @@ class Hourly
   def convert_degrees_to_direction(degree)
     selector = (((degree - 11.25) / 22.5) + 0.5).round
     direction_array = ["N", "NNE", "NE", "ENE", "E", "ESE",  "SE",  "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
-    if degree < 11.25
-      direction_array[selector.abs - 1]
-    else
-      direction_array[selector.abs]
-    end
+    direction_array[(selector.abs % 16)]
   end
 end
--- spec/poros/hourly_spec.rb
+++ spec/poros/hourly_spec.rb
@@ -22,4 +22,29 @@ RSpec.describe Hourly, :vcr do
     expect(hourly.first.conditions).to eq('few clouds')
     expect(hourly.first.icon).to eq('02d')
   end
+
+  it "can convert degrees to direction" do
+    location = 'denver, co'
+    map = MapFacade.get_coords_by_loc(location)
+    json = ForecastService.forecast_by_coords(map)
+    details = json[:hourly].first
+    hourly = Hourly.new(details)
+    expect(hourly.convert_degrees_to_direction(0)).to eq('N')
+    expect(hourly.convert_degrees_to_direction(12)).to eq('NNE')
+    expect(hourly.convert_degrees_to_direction(34)).to eq('NE')
+    expect(hourly.convert_degrees_to_direction(57)).to eq('ENE')
+    expect(hourly.convert_degrees_to_direction(79)).to eq('E')
+    expect(hourly.convert_degrees_to_direction(102)).to eq('ESE')
+    expect(hourly.convert_degrees_to_direction(124)).to eq('SE')
+    expect(hourly.convert_degrees_to_direction(147)).to eq('SSE')
+    expect(hourly.convert_degrees_to_direction(169)).to eq('S')
+    expect(hourly.convert_degrees_to_direction(192)).to eq('SSW')
+    expect(hourly.convert_degrees_to_direction(214)).to eq('SW')
+    expect(hourly.convert_degrees_to_direction(237)).to eq('WSW')
+    expect(hourly.convert_degrees_to_direction(259)).to eq('W')
+    expect(hourly.convert_degrees_to_direction(282)).to eq('WNW')
+    expect(hourly.convert_degrees_to_direction(304)).to eq('NW')
+    expect(hourly.convert_degrees_to_direction(327)).to eq('NNW')
+    expect(hourly.convert_degrees_to_direction(349)).to eq('N')
+  end
 end
