b72bf887932475a3566c99dbce6575a6e18a0c89
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-10 18:25:49 -0700

    Refactor road trip controller for sad path
    - Adds condition for facade return
    - If class is RoadTrip, then sucess
    - Else, return custom errors
    - Line 9 else occurs when origin or destination have errors
    - Line 11 else occurs when api key is invalid

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -3,9 +3,13 @@ class Api::V1::RoadTripController < ApplicationController
     authenticated_user = User.find_by(api_key: road_trip_params[:api_key])
     if authenticated_user
       road_trip = RoadTripFacade.create_trip(road_trip_params[:origin], road_trip_params[:destination])
-      render json: RoadtripSerializer.new(road_trip)
+      if road_trip.class == RoadTrip
+        render json: RoadtripSerializer.new(road_trip)
+      else
+        render body: "ERROR: #{road_trip}", status: 400
+      end
     else
-      render body: nil, status: 404 # Put in custom error section
+      render body: 'ERROR: Invalid Api Key', status: 401
     end
   end
 
